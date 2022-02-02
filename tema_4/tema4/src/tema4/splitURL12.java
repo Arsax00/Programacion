3b1cbdac8131e02390dd0e28a762b1f229d3fc64
Arsax00 <sara.isabel.jordana@gmail.com>
2022-02-02 08:59:50 +0100

    Create splitURL12.java

---
+++ tema_4/tema4/src/tema4/splitURL12.java
@@ -0,0 +1,21 @@
+package tema4;
+
+public class splitURL12 {
+
+	public static void main(String[]args) {
+		
+	//	String URL="http://www.medac.es";
+	//	String newURL = reemplazar(URL,"medac","itep");
+	//	System.out.println(newURL);
+		
+		String str= "huele un poco a caca de vaca ->";
+		
+		String str1= str.replace("c", "C");
+		System.out.println(str1);
+		
+	}
+		
+		
+		
+	
+}
