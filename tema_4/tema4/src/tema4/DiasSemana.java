fa2b77c4aff28f61e44083e8f3fe2656d3d80401
Arsax00 <sara.isabel.jordana@gmail.com>
2022-02-02 09:05:49 +0100

    Create DiasSemana.java

---
+++ tema_4/tema4/src/tema4/DiasSemana.java
@@ -0,0 +1,18 @@
+package tema4;
+
+public class DiasSemana{
+
+	//Enumeraciones "DiasSemana.java"
+	//JAVA Ejercicio1Trim2
+	DiasSemana dias = new DiasSemana();
+	public enum DiasSem{
+		LUNES,
+		MARTES,
+		MIERCOLES,
+		JUEVES,
+		VIERNES,
+		SABADO,
+		DOMINGO;
+	}
+	
+}
