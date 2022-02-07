b30f3bb561cb7dbaac1bd304f5a74bbccb754600
Arsax00 <sara.isabel.jordana@gmail.com>
2022-02-07 10:25:46 +0100

    Update ClasePrincipalPreExamen.java

--- tema_4/src/tema_4/ClasePrincipalPreExamen.java
+++ tema_4/src/tema_4/ClasePrincipalPreExamen.java
@@ -5,16 +5,27 @@ public class ClasePrincipalPreExamen {
 	public static void main (String[]args){
 		
 		
-		Human obj= new Human();
+		Human human1= new Human("Josh");	
 		
-		System.out.println("El humano se llama: "+obj.getName());		
+		human1.walk();
 		
-		obj.respirar();
-		obj.comer();
-		obj.walk();
+		LivingT livingthing1=human1;
+		livingthing1.walk();
 		
+		LivingT livingthing2= new Monkey("MonkeyWrench");
+		livingthing2.walk();
+		
+		
+		System.out.println("human1.getName() = " + human1.getName());
+		System.out.println("livingthing1.getName() = " + livingthing1.getName());
+		
+		boolean b1 = (human1 == livingthing1);
+		System.out.println("Do human1 and livingthing1 point to the same object instance? " + b1);
+		
+		//LivingT z = new LivingT(); 
 		
 	}
 	
-
+	
+	 
 }
