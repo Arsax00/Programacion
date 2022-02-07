8c1fa3a8036c918e72e4d71c8436e71348e051b2
Arsax00 <sara.isabel.jordana@gmail.com>
2022-02-07 09:11:13 +0100

    Create ClasePrincipalPreExamen.java

---
+++ tema_4/src/tema_4/ClasePrincipalPreExamen.java
@@ -0,0 +1,20 @@
+package tema_4;
+
+public class ClasePrincipalPreExamen {
+	
+	public static void main (String[]args){
+		
+		
+		Human obj= new Human();
+		
+		System.out.println("El humano se llama: "+obj.getName());		
+		
+		obj.respirar();
+		obj.comer();
+		obj.walk();
+		
+		
+	}
+	
+
+}
