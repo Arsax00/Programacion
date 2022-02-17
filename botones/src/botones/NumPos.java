5d1995d2b18a864028d8d5c5c4cee85be8a326f6
Arsax00 <sara.isabel.jordana@gmail.com>
2022-02-17 11:00:27 +0100

    Create NumPos.java

---
+++ botones/src/botones/NumPos.java
@@ -0,0 +1,24 @@
+package botones;
+
+public class NumPos {
+
+public static void main() {
+	int pos=4;
+	
+	
+	if(pos>=0) {
+	
+	System.out.println(pos+" es positivo");	
+	
+	
+	}else {
+		
+		System.out.println(pos+" es negativo");
+		
+		
+		
+	}
+	
+	
+}	
+}
