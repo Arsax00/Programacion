84a11162d763c4aa5badd6d625b2d64bc694456f
Arsax00 <57747785+Arsax00@users.noreply.github.com>
2022-11-03 12:49:36 +0100

    Create main.java

---
+++ Hilos/src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+       RafaNadal rf = new RafaNadal();
+       Djokovic dj= new Djokovic();
+       
+       rf.start();
+       dj.start();
+       
+      
+     
+	}
+
+}
